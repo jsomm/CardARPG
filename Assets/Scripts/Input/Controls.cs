37d2372b08c85b36fac19b1c4bef771be5a861d5
Jake Sommermeyer <jakesommermeyer@gmail.com>
2021-09-23 16:17:58 -0400

    Basic movement and animation
    Every commit from this point forward will work with multiplayer. Starting off on the right foot this time...

---
+++ Assets/Scripts/Input/Controls.cs
@@ -0,0 +1,202 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerControls"",
+            ""id"": ""792c0c3a-7de0-41a9-bf6f-b49a32fbe9cb"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""87bec06b-7c49-4c48-826a-ec0b9ebf48df"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""42d73e4a-0090-4033-8b72-c9c1acf7c5eb"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""9a702073-8bca-4dab-96aa-b4cbc53b1561"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse and Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""f2fb544d-e0b1-43f2-b63a-9966b0052de0"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse and Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""60a4a019-16dd-420b-85b2-cdee39399645"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse and Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""049a1180-486a-43f5-bac8-718b7b955558"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Mouse and Keyboard"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Mouse and Keyboard"",
+            ""bindingGroup"": ""Mouse and Keyboard"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // PlayerControls
+        m_PlayerControls = asset.FindActionMap("PlayerControls", throwIfNotFound: true);
+        m_PlayerControls_Movement = m_PlayerControls.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerControls
+    private readonly InputActionMap m_PlayerControls;
+    private IPlayerControlsActions m_PlayerControlsActionsCallbackInterface;
+    private readonly InputAction m_PlayerControls_Movement;
+    public struct PlayerControlsActions
+    {
+        private @Controls m_Wrapper;
+        public PlayerControlsActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_PlayerControls_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerControlsActions instance)
+        {
+            if (m_Wrapper.m_PlayerControlsActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public PlayerControlsActions @PlayerControls => new PlayerControlsActions(this);
+    private int m_MouseandKeyboardSchemeIndex = -1;
+    public InputControlScheme MouseandKeyboardScheme
+    {
+        get
+        {
+            if (m_MouseandKeyboardSchemeIndex == -1) m_MouseandKeyboardSchemeIndex = asset.FindControlSchemeIndex("Mouse and Keyboard");
+            return asset.controlSchemes[m_MouseandKeyboardSchemeIndex];
+        }
+    }
+    public interface IPlayerControlsActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using Mirror;
+using UnityEngine.InputSystem;
+using System;
+
+public class PlayerMovementController : NetworkBehaviour
+{
+    [SerializeField] float _movementSpeed = 5f;
+    [SerializeField] Animator _animator;
+
+    CharacterController _characterController;
+
+    int _isRunningHash;
+    Vector3 _previousInput;
+    bool _isMovementPressed = false;
+
+    Controls _controls;
+    public Controls Controls
+    {
+        get
+        {
+            if (_controls != null) { return _controls; }
+            return _controls = new Controls();
+        }
+    }
+
+    public override void OnStartAuthority()
+    {
+        enabled = true;
+
+        _characterController = GetComponent<CharacterController>();
+        _isRunningHash = Animator.StringToHash("isRunning");
+
+        Controls.PlayerControls.Movement.performed += ctx => SetMovement(ctx.ReadValue<Vector2>());
+        Controls.PlayerControls.Movement.canceled += ctx => ResetMovement();
+    }
+
+    [ClientCallback]
+    private void OnEnable() => Controls.Enable();
+    [ClientCallback]
+    private void OnDisable() => Controls.Disable();
+
+    [Client]
+    private void Update() => Move();
+
+    private void SetMovement(Vector2 movement)
+    {
+        _previousInput = new Vector3(movement.x, 0f, movement.y);
+        _isMovementPressed = true;
+    }
+
+    private void ResetMovement()
+    {
+        _previousInput = Vector2.zero;
+        _isMovementPressed = false;
+    }
+
+    private void Move()
+    {
+        _characterController.Move(_previousInput * _movementSpeed * Time.deltaTime);
+        if (_isMovementPressed)
+        {
+            HandleRotation();
+        }
+
+        HandleAnimation();
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = _previousInput.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = _previousInput.z;
+
+        Quaternion currentRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+
+        transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, 15f * Time.deltaTime); // change '15f' to adjust how quickly the player rotates
+        _animator.gameObject.transform.rotation = transform.rotation;
+    }
+
+    private void HandleAnimation()
+    {
+        // more complicated move animations could go here, but this is all we need for now
+        _animator.SetBool(_isRunningHash, _isMovementPressed);
+    }
+}
